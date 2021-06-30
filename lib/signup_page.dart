fcb84b6feea5d401477582cf0d7a014cb625d636
amartyanambiar <amartyanambiar@gmail.com>
2021-06-30 19:28:03 +0530

    1

---
+++ lib/signup_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class signup extends StatefulWidget {
+  const signup({Key? key}) : super(key: key);
+
+  @override
+  _signupState createState() => _signupState();
+}
+
+class _signupState extends State<signup> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
